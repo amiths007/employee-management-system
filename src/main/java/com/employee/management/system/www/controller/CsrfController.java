e3e44c4bba02507723db8718fc698e5788582f5c
amiths007 <amithsammi8@gmail.com>
2025-02-09 12:55:46 +0530

    spring security-impl

---
+++ src/main/java/com/employee/management/system/www/controller/CsrfController.java
@@ -0,0 +1,15 @@
+package com.employee.management.system.www.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CsrfController {
+
+    @GetMapping("/get/csrf_token")
+    public CsrfToken getCsrfToken(HttpServletRequest httpServletRequest) {
+        return (CsrfToken) httpServletRequest.getAttribute("_csrf");
+    }
+}
